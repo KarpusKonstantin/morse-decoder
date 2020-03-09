ebacf1972a9db55c7bb73ae4f4196d258ad7f4a2
KarpusKonstantin <60568968+KarpusKonstantin@users.noreply.github.com>
2020-03-09 12:53:50 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = [];
+    let reuslt = [];
+    let str = '';
+    let str2 = '';
+    let numChar = expr.length / 10;
+
+//    console.log('numChar = ', numChar);
+//    console.log(expr);
+
+    for (let i = 1; i <= numChar; i++) {
+        str = expr.slice((i * 10) - 10, (i * 10));
+        array.push(str);
+    }
+
+//    console.log(array);
+
+    for (let i = 0; i < array.length; i++ ) {
+        str = array[i];
+        for (let j = 0; j < str.length; j++ ) {
+            if (str[j] === '*') {
+                str2 = str2 + ' ';
+                break;
+            }
+
+            if (str[j] !== '0') {
+                //reuslt.push(str.slice(j).replace(/(10)/g,'.').replace(/(11)/g,'-'));
+                str2 = str2 + MORSE_TABLE[str.slice(j).replace(/(10)/g,'.').replace(/(11)/g,'-')];
+                break;
+            }
+        }
+    }
+
+//    console.log(reuslt);
+//    console.log(str2);
+
+    return str2;
+
 }
 
 module.exports = {
